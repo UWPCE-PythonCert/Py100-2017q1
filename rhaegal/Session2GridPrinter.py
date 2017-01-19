fb2425630049d14391c8025086df30048490aaea
cardinalorange <cardinalorange@gmail.com>
2017-01-19 04:59:06 +0000

    Fixing missed variable Sessions 2 Grid Printer

--- rhaegal/Session2GridPrinter.py
+++ rhaegal/Session2GridPrinter.py
@@ -1,6 +1,6 @@
 size=5
 for line in range(11):
     if line%size==0:
-        print "+"+"-"*5+"+"+"-"*5+"+"
+        print "+"+"-"*size+"+"+"-"*size+"+"
     else:
-        print "|"+" "*5+"|"+" "*5+"|"
+        print "|"+" "*size+"|"+" "*size+"|"
