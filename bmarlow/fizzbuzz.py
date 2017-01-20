39d06af9c8f8872091b2ee18f0b69c0277a8cb01
buddinski <brandon.n.marlow@gmail.com>
2017-01-20 07:31:03 -0800

    Updating FizzBuzz
    Changed the approach to fizzbuzz

--- bmarlow/fizzbuzz.py
+++ bmarlow/fizzbuzz.py
@@ -1,5 +1,7 @@
 for i in range(1,101):
 	
+#attempt number 1 (which worked)
+	'''
 	if i % 3 == 0 and i % 5 == 0:
 		print('FizzBuzz')
 	elif i % 3 == 0:
@@ -8,3 +10,14 @@ for i in range(1,101):
 		print('Buzz')
 	else:
 		print(i)
+	'''
+
+#attempt number two, which is a bit neater
+	count = ''
+	if i % 3 == 0:
+		count += 'Fizz'
+	if i % 5 == 0:
+		count += 'Buzz'
+	if count == '':
+		count = i
+	print(count)
