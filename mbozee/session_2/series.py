859872dcc80e5c2b5a122e7b141744593dab557e
Mike Bozee <msbozee@gmail.com>
2017-01-21 20:38:54 +0000

    Series.py added: fib, lucas, sum_series

--- mbozee/session_2/series.py
+++ mbozee/session_2/series.py
@@ -15,3 +15,28 @@ print(fibonacci(10))
 
 def lucas(n):
     """Return fibonacci series."""
+    if type(n) != int:
+        print("n must be an integer!")
+    elif n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-2) + lucas(n-1)
+
+print(lucas(10))
+
+
+def sum_series(n, val1=0, val2=1):
+    """Return a series."""
+    if type(n) != int:
+        print("n must be an integer!")
+    elif n == 0:
+        return val1
+    elif n == 1:
+        return val2
+    else:
+        return sum_series(n-2) + sum_series(n-1)
+
+# ERROR: This does not produce Lucas number.
+print(sum_series(10, 2, 1))
