5de6515f302d4d4aaaac6cb57c88a1e48dcf1513
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-21 03:47:49 -0800

    Grin Printer 02

---
+++ vzzjav/Session_02/Exercises/Grid_Printer_02.py
@@ -0,0 +1,15 @@
+# Javier Vazquez
+# Grid Printer
+# Jan 21, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/grid_printer.html
+
+def print_grid(n):
+    pass
+
+def main():
+    n = input("Size of grid: ")
+    print_grid(n)
+
+
+if __name__ == '__main__':
+    main()
