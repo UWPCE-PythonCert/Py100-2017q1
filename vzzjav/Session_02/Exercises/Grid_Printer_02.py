ae2c06925e28fc4a68c0891edf00c87999f7a1ab
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-21 03:54:28 -0800

    Grin Printer 02

--- vzzjav/Session_02/Exercises/Grid_Printer_02.py
+++ vzzjav/Session_02/Exercises/Grid_Printer_02.py
@@ -4,7 +4,29 @@
 # Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/grid_printer.html
 
 def print_grid(n):
-    pass
+    line = ""
+    for i in range(11):
+        for j in range(21):
+            if i % 5 == 0:
+                if j % 10 == 0:
+                    if j == 20:
+                        line +="+\n"
+                    else:
+                        line += "+"
+                elif j % 2 == 0 and (j != 10 or j != 20):
+                    line +="-"
+                else:
+                    line +=" "
+            else:
+                if j % 10 == 0:
+                    if j == 20:
+                        line +="|\n"
+                    else:
+                        line += "|"
+                else:
+                    line +=" "
+
+    print(line)
 
 def main():
     n = input("Size of grid: ")
