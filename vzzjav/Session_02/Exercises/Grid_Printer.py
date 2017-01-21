a9ebd1b16bad8240f506c8d25cfddf98cc981a0d
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-21 02:17:54 -0800

    on the way

--- vzzjav/Session_02/Exercises/Grid_Printer.py
+++ vzzjav/Session_02/Exercises/Grid_Printer.py
@@ -10,21 +10,24 @@ def main():
         for j in range(21):
             if i == 0 or i == 5 or i == 10:
                 if j == 0 or j == 10 or j == 20:
-                    line +="+"
+                    if j == 20:
+                        line +="+\n"
+                    else:
+                        line += "+"
                 elif j % 2 == 0 and (j != 10 or j != 20):
                     line +="-"
                 else:
                     line +=" "
-            line += "\n"
+            else:
+                if j == 0 or j == 10 or j == 20:
+                    if j == 20:
+                        line +="|\n"
+                    else:
+                        line += "|"
+                else:
+                    line +=" "
+
     print(line)
-
-
-
-
-
-
-    # print("{0:<20}{1:>5}{2:<5}{3:<20}{4:>5}"
-    #       .format([i])
 
 if __name__ == '__main__':
     main()
