4307ab68ec074bf446036136f8ddd91d7b9e9ef1
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-23 08:53:51 -0800

    fibonacci serie in PROCESS!!!

---
+++ vzzjav/Session_02/Exercises/series.py
@@ -0,0 +1,10 @@
+# Javier Vazquez
+# Grid Printer
+# Jan 23, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/fib_and_lucas.html
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
