44f1279b7c6ea30ea5b2a2939445e0a02dbc341f
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-23 07:55:34 -0800

    fizzBuzz in process!

---
+++ vzzjav/Session_02/Exercises/fizzBuzz.py
@@ -0,0 +1,10 @@
+# Javier Vazquez
+# Grid Printer
+# Jan 23, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/grid_printer.html
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
