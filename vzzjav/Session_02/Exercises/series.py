07e5e1398fc7ff1d4dd919c424cac58272b90cc8
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-23 09:49:14 -0800

    fibonacci serie in WORKS OK!!!

--- vzzjav/Session_02/Exercises/series.py
+++ vzzjav/Session_02/Exercises/series.py
@@ -7,19 +7,34 @@ def fib(n):
     if n <= 1:
         return n
     else:
-        return (fib(n-1) + fib(n-2))
+        return (fib(n-2) + fib(n-1))
+
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return (lucas(n-2) + lucas(n-1))
 
 def main():
-    serie = ""
+    serief = ""
+    seriel = ""
     print("Number of iterations must be greater than 0")
     n = input("Number of iterations: ")
     n = int(n)
     if n <= 0:
         print("Number of iterations must be greater than 0")
     else:
+        print("Fibonacci")
         for i in range(n):
-            serie += str(fib(i)) + ", "
-    print(serie)
+            serief += str(fib(i)) + ", "
+        print(serief)
+
+        print("\nLucas")
+        for i in range(n):
+            seriel += str(lucas(i)) + ", "
+        print(seriel)
 
 if __name__ == '__main__':
     main()
