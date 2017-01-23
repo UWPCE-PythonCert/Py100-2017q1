c835cd26914e053a89a2ed597fd93fe0714b51e6
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-23 09:09:24 -0800

    fibonacci serie in PROCESS!!!

--- vzzjav/Session_02/Exercises/series.py
+++ vzzjav/Session_02/Exercises/series.py
@@ -3,8 +3,23 @@
 # Jan 23, 2017
 # Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/fib_and_lucas.html
 
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return (fib(n-1) + fib(n-2))
+
 def main():
-    pass
+    serie = ""
+    print("Number of iterations must be greater than 0")
+    n = input("Number of iterations: ")
+    n = int(n)
+    if n <= 0:
+        print("Number of iterations must be greater than 0")
+    else:
+        for i in range(n):
+            serie += str(fib(i)) + ", "
+    print(serie)
 
 if __name__ == '__main__':
     main()
