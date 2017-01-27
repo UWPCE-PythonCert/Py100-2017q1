a6def7946f3717630ebd415a2225709c677bbadd
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-26 18:12:14 -0800

    IN PROGRESS

---
+++ vzzjav/Session_03/Exercises/slicingLab.py
@@ -0,0 +1,15 @@
+# Javier Vazquez
+# Grid Printer
+# Jan 26, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/slicing.html
+
+import string
+
+def exchangeFL(sequence):
+
+
+def main():
+    alphabet = string.ascii_lowercase
+
+if __name__ == '__main__':
+    main()
