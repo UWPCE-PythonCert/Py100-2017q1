92bd7281e4a92ad6215177249bc82fcafbae3772
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-27 03:54:20 -0800

    IN PROGRESS

--- vzzjav/Session_03/Exercises/slicingLab.py
+++ vzzjav/Session_03/Exercises/slicingLab.py
@@ -3,56 +3,44 @@
 # Jan 26, 2017
 # Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/slicing.html
 
-import string
+import string, math
 
 def exchangeFL(sequence):
     if isinstance(sequence, str):
-        outputSeq = ""
-        for i in range(len(sequence)):
-            if i == 0:
-                outputSeq += sequence[len(sequence)-1]
-            elif i == len(sequence) - 1:
-                outputSeq += sequence[0]
-            else:
-                outputSeq += sequence[i]
-        return outputSeq
+        return sequence[-1] + sequence[1:len(sequence)-1] + sequence[0]
 
-def removedItem(sequence, n):
+def removedItem(sequence):
     if isinstance(sequence, str):
-        outputSeq = ""
-        for i in range(len(sequence)):
-            if i < n-1:
-                outputSeq += sequence[i]
-            elif i == n-1:
-                outputSeq += ""
-            elif i > n-1:
-                outputSeq += sequence[i]
-        return outputSeq
+        return sequence[0:len(sequence):2]
+
 
 def remover4fl_n(sequence, n):
     if isinstance(sequence, str):
-        outputSeq = ""
-        for i in range(4, len(sequence)-4):
-            if i == n-1:
-                outputSeq += ""
-            else:
-                outputSeq += sequence[i]
-        return outputSeq
+        return sequence[4:-4:1]
 
 def reverse(sequence):
     if isinstance(sequence, str):
-        outputSeq = ""
-        for i in range(len(sequence), -1, -1):
-            outputSeq += str(i)
-        return outputSeq
+        return sequence[::-1]
+
+def thirds(sequence):
+    if isinstance(sequence, str):
+        if len(sequence) % 3 == 0:
+            return sequence[len(sequence)/3:(len(sequence)/3)*2]
+        else:
+            return sequence[math.ceil(len(sequence) / 3):math.ceil((len(sequence) / 3) * 2)] + \
+                   sequence[math.ceil((len(sequence) / 3) * 2):] + \
+                   sequence[0:math.ceil(len(sequence) / 3)]
+
+
 
 
 
 def main():
     print(exchangeFL(string.ascii_lowercase))
-    print(removedItem(string.ascii_lowercase, 10))
+    print(removedItem(string.ascii_lowercase))
     print(remover4fl_n(string.ascii_lowercase, 10))
     print(reverse(string.ascii_lowercase))
+    print(thirds(string.ascii_lowercase))
 
 if __name__ == '__main__':
     main()
