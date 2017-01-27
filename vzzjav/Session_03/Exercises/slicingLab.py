bf5c4514d87b154ba3a071e85e43a734bbb868fb
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-26 20:24:46 -0800

    IN PROGRESS

--- vzzjav/Session_03/Exercises/slicingLab.py
+++ vzzjav/Session_03/Exercises/slicingLab.py
@@ -6,10 +6,53 @@
 import string
 
 def exchangeFL(sequence):
+    if isinstance(sequence, str):
+        outputSeq = ""
+        for i in range(len(sequence)):
+            if i == 0:
+                outputSeq += sequence[len(sequence)-1]
+            elif i == len(sequence) - 1:
+                outputSeq += sequence[0]
+            else:
+                outputSeq += sequence[i]
+        return outputSeq
+
+def removedItem(sequence, n):
+    if isinstance(sequence, str):
+        outputSeq = ""
+        for i in range(len(sequence)):
+            if i < n-1:
+                outputSeq += sequence[i]
+            elif i == n-1:
+                outputSeq += ""
+            elif i > n-1:
+                outputSeq += sequence[i]
+        return outputSeq
+
+def remover4fl_n(sequence, n):
+    if isinstance(sequence, str):
+        outputSeq = ""
+        for i in range(4, len(sequence)-4):
+            if i == n-1:
+                outputSeq += ""
+            else:
+                outputSeq += sequence[i]
+        return outputSeq
+
+def reverse(sequence):
+    if isinstance(sequence, str):
+        outputSeq = ""
+        for i in range(len(sequence), -1, -1):
+            outputSeq += str(i)
+        return outputSeq
+
 
 
 def main():
-    alphabet = string.ascii_lowercase
+    print(exchangeFL(string.ascii_lowercase))
+    print(removedItem(string.ascii_lowercase, 10))
+    print(remover4fl_n(string.ascii_lowercase, 10))
+    print(reverse(string.ascii_lowercase))
 
 if __name__ == '__main__':
     main()
