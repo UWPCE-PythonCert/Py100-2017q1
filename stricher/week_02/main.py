e07482e5a0b7428736e104165b2c234b2cd1f13a
vian-st <vian-st@users.noreply.github.com>
2017-01-27 23:33:50 +0100

    Add ExercisesToStdout() in PrintExercises.py to print the homework exercises to stdout. Add ExercisesToStdout.allExercises_stdout() to main() after RunAllTests().launchAll().

--- stricher/week_02/main.py
+++ stricher/week_02/main.py
@@ -3,11 +3,18 @@
 
 def main():
 
+    # ---------- Run All Tests --------
+
     # unittest library needed
 
     from runAllTests import RunAllTests
     RunAllTests().launchAll()
 
+    # -------- Homework exercises --------
+
+    from PrintExercises import ExercisesToStdout
+
+    ExercisesToStdout.allExercises_stdout()
 
 if __name__ == "__main__":
 	main()
