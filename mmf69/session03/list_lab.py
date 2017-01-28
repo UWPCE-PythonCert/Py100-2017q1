4092370b9068a1a49d25c1079cadf7c853835b1f
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-01-28 14:17:16 -0800

    Add list_lab.py

---
+++ mmf69/session03/list_lab.py
@@ -0,0 +1,17 @@
+'''---------------------------------------------------
+List Lab Part 1
+Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+Display the list.
+Ask the user for another fruit and add it to the end of the list.
+Display the list.
+Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis).
+Add another fruit to the beginning of the list using “+” and display the list.
+Add another fruit to the beginning of the list using insert() and display the list.
+Display all the fruits that begin with “P”, using a for loop.
+---------------------------------------------------'''
+
+#Create list of fruit
+fruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
+print (fruit, "\n")
+
+
