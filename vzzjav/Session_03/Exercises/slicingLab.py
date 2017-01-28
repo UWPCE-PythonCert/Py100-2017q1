870660492b582537a285f8fb8f4b89d7dc6ce400
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-28 00:06:43 -0800

    IN PROGRESS

--- vzzjav/Session_03/Exercises/slicingLab.py
+++ vzzjav/Session_03/Exercises/slicingLab.py
@@ -22,13 +22,20 @@ def remover4fl(sequence):
         return sequence[4:-4:1]
 
 def reverse(sequence):
+    '''Return a sequence reversed (just with slicing)'''
     if isinstance(sequence, str):
         return sequence[::-1]
 
 def thirds(sequence):
+    '''Return a sequence with the middle third, then last third, then the first third in the new order'''
     if isinstance(sequence, str):
         if len(sequence) % 3 == 0:
-            return sequence[len(sequence)/3:(len(sequence)/3)*2]
+            return sequence[len(sequence)/3:(len(sequence)/3)*2] + \
+                   sequence[math.ceil((len(sequence) / 3) * 2):] + \
+                   sequence[0:math.ceil(len(sequence) / 3)]
+
+
+
         else:
             return sequence[math.ceil(len(sequence) / 3):math.ceil((len(sequence) / 3) * 2)] + \
                    sequence[math.ceil((len(sequence) / 3) * 2):] + \
@@ -45,7 +52,9 @@ def main():
     print(removedItem(string.ascii_lowercase),"\n")
     print(remover4fl.__doc__)
     print(remover4fl(string.ascii_lowercase),"\n")
+    print(reverse.__doc__)
     print(reverse(string.ascii_lowercase),"\n")
+    print(thirds.__doc__)
     print(thirds(string.ascii_lowercase),"\n")
 
 if __name__ == '__main__':
