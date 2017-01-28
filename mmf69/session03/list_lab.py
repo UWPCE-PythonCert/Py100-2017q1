a9c929f913ec7eb033afe90e0123f44c3cc26984
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-01-28 15:15:55 -0800

    Remove 'while' loop that was causing issues. Add fruit to front of list using "+"

--- mmf69/session03/list_lab.py
+++ mmf69/session03/list_lab.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 '''---------------------------------------------------
 List Lab Part 1
 Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
@@ -16,13 +18,18 @@ print(fruit, "\n")
 
 
 #Ask the user for another fruit and add it to the end of the list.
-fruit.append(input("Pleaae add a fruit to the list:\n"))
+fruit.append(input("Please add a fruit to the list:\n"))
 print(fruit, "\n")
 
 #Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis).
-while True:
-    try:
-        number = int(input("Pick a number:\n"))
-        print("The number {} corresponds to {} in the list.\n".format(number,fruit[number - 1]))
-    except IndexError:
-        print("That number is not in the list")
+try:
+    number = int(input("Pick a number:\n"))
+    print("The number {} corresponds to {} in the list.\n".format(number,fruit[number - 1]))
+except IndexError:
+    print("That number is not in the list")
+
+#Add another fruit to the beginning of the list using “+” and display the list.
+print("Now I will add Mango to the front of the list.\n")
+fruit = ["Mango"]+fruit
+print(fruit, "\n")
+
