f1e3bcccd498cd4162c163b446e7578b0f9319be
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-01-28 18:26:51 -0800

    Add slicing_lab

---
+++ mmf69/session03/slicing_lab.py
@@ -0,0 +1,18 @@
+'''------------------------------------------------------------------------------------------------
+Slicing Lab
+return a sequence with the first and last items exchanged.
+return a sequence with every other item removed
+return a sequence with the first and last 4 items removed, and every other item in between
+return a sequence reversed (just with slicing)
+return a sequence with the middle third, then last third, then the first third in the new order
+-----------------------------------------------------------------------------------------------'''
+
+#return a sequence with the first and last items exchanged.
+s =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(s)
+
+print(len(s))
+
+
+#return a sequence reversed (just with slicing)return a sequence reversed (just with slicing)
+print(list(reversed(s)))
