025290260d362c8067674467e7085cb8c2342134
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-01-29 14:17:32 -0800

    Add reverse sequence

--- mmf69/session03/slicing_lab.py
+++ mmf69/session03/slicing_lab.py
@@ -7,12 +7,20 @@ return a sequence reversed (just with slicing)
 return a sequence with the middle third, then last third, then the first third in the new order
 -----------------------------------------------------------------------------------------------'''
 
-#return a sequence with the first and last items exchanged.
-s =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+s =  "What is that dog's name?"
 print(s)
-
 print(len(s))
 
+#return a sequence with the first and last items exchanged.
+print(s[23] + s[1:22]  + s[0])
+
+#return a sequence with every other item removed
+print(s[::2])
 
 #return a sequence reversed (just with slicing)return a sequence reversed (just with slicing)
-print(list(reversed(s)))
+print(str(s[::-1]))
+
+#return a sequence with the middle third, then last third, then the first third in the new order
+
+
+
