9a5ee7fb0a4f63310fce53d82527bf489aa5d2a4
Mike Bozee <msbozee@gmail.com>
2017-01-29 14:52:51 +0000

    Thank You letter added

--- mbozee/session_3/mailroom.py
+++ mbozee/session_3/mailroom.py
@@ -11,14 +11,59 @@ donors = {
 
 def thank_you():
     """Write a thank you letter."""
-    print('test')
+    full_name = input("(Donor full name) / (list) > ")
+
+    if full_name == 'list'.lower():
+        print("Donors:", end=' ')
+        for key, value in donors.items():
+            print(key, end=', ')
+        print('\n')
+        thank_you()
+    elif full_name in donors:
+        new_donation = int(input("New donation amount > "))
+        donors[full_name].append(new_donation)
+        print(donors)
+        write_letter(full_name, new_donation)
+    else:
+        donors[full_name] = []
+        new_donation = int(input("New donation amount > "))
+        donors[full_name].append(new_donation)
+        print(donors)
+        write_letter(full_name, new_donation)
+
+
+def write_letter(full_name, new_donation):
+    """Compose and print thank you letter."""
+    letter = '''
+    Dear {},
+
+    Thank you for your generous donation of ${}.
+    Your gift makes a major impact on aspiring musicians in the Seattle area.
+
+    Sincerely,
+    Seattle Music Fund
+    '''.format(full_name, str(new_donation))
+    print(letter)
+    menu()
+
+
+def create_report():
+    """Generate a report of donors and donation amounts."""
+    pass
 
 
 def menu():
     """Give user choice of actions."""
     options = {1: 'Send a Thank You', 2: 'Create a Report', 3: 'quit'}
     choice = input("Choose an option:\n" + str(options) + " > ")
+
     if choice == str(1):
         thank_you()
+    elif choice == str(2):
+        print("Create a Report coming soon...")
+    elif choice == str(3):
+        pass
+    else:
+        menu()
 
 menu()
