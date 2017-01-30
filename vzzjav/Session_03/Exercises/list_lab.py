8f01fdf7c84c84817483dd8ffcb12afd3bd0b5d7
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-30 02:24:16 -0800

    IN PROGRESS!!!!

--- vzzjav/Session_03/Exercises/list_lab.py
+++ vzzjav/Session_03/Exercises/list_lab.py
@@ -8,43 +8,43 @@ import os
 def main():
     '''List's exercise'''
     fruits = ["Apples", "Pears", "Oranges", "Peaches"]
-    # print(fruits)
-    # fruits.append(input("Add fruit: "))
-    # print(fruits)
-    # print(fruits[int(input("Element to display: "))-1])
-    # fruits = [input("Add fruit: ")] + fruits
-    # print(fruits)
-    # fruits.insert(0, input("Add fruit: "))
-    # print(fruits)
-    # [print(fruit) for fruit in fruits if "P" in fruit[0]]
-    # print(fruits)
+    print(fruits)
+    fruits.append(input("Add fruit: "))
+    print(fruits)
+    print(fruits[int(input("Element to display: "))-1])
+    fruits = [input("Add fruit: ")] + fruits
+    print(fruits)
+    fruits.insert(0, input("Add fruit: "))
+    print(fruits)
+    [print(fruit) for fruit in fruits if "P" in fruit[0]]
+    print(fruits)
     fruitsS1 = fruits[:]
-    # del fruits[len(fruits)-1]
-    # fruits *= 2
-    # print(fruits)
-    # fruitRemove = input("Remove fruit: ")
-    # [fruits.remove(fruitRemove) for fruit in fruits if fruitRemove in fruits]
-    # print(fruits)
-    # fruits = fruitsS1[:]
-    # print("Fruits serie 1: ", fruits)
-    # i = 0
-    # removeList = []
-    # for fruit in fruits:
-    #     while True:
-    #         answer = input("Do you like " + fruit + "?")
-    #         if answer == "yes":
-    #             fruits[i] = fruit.lower()
-    #             print(fruits[i])
-    #             break
-    #         elif answer == "no":
-    #             removeList.append(fruit)
-    #             break
-    #         else:
-    #             print("Please just answer [yes] or [no].")
-    #             continue
-    #     i += 1
-    # for fruit in removeList:
-    #     fruits.remove(fruit)
+    del fruits[len(fruits)-1]
+    fruits *= 2
+    print(fruits)
+    fruitRemove = input("Remove fruit: ")
+    [fruits.remove(fruitRemove) for fruit in fruits if fruitRemove in fruits]
+    print(fruits)
+    fruits = fruitsS1[:]
+    print("Fruits serie 1: ", fruits)
+    i = 0
+    removeList = []
+    for fruit in fruits:
+        while True:
+            answer = input("Do you like " + fruit + "?")
+            if answer == "yes":
+                fruits[i] = fruit.lower()
+                print(fruits[i])
+                break
+            elif answer == "no":
+                removeList.append(fruit)
+                break
+            else:
+                print("Please just answer [yes] or [no].")
+                continue
+        i += 1
+    for fruit in removeList:
+        fruits.remove(fruit)
     print(fruits)
     fruits = fruitsS1[:]
     print("Fruits serie 1: ", fruits)
