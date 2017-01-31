84da997193b3e29fdd85ca1d7f2436b9e4462a7d
Javier Vazquez <vzz_jav@yahoo.com>
2017-01-31 03:49:10 -0800

    IN PROGRESS!!!!

--- vzzjav/Session_03/Exercises/stringFormatting.py
+++ vzzjav/Session_03/Exercises/stringFormatting.py
@@ -2,3 +2,17 @@
 # Grid Printer
 # Jan 30, 2017
 # Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/string_formatting.html
+
+import os
+
+def formatingString(*args):
+    pass
+
+def main():
+    '''String formatting'''
+    pass
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__))
+    print(main.__doc__)
+    main()
