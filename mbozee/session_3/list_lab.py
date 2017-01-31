2175ce2e01a89987c6c6d1f3cd36bb719a0adbb1
Mike Bozee <msbozee@gmail.com>
2017-01-31 03:26:10 +0000

    restore

--- mbozee/session_3/list_lab.py
+++ mbozee/session_3/list_lab.py
@@ -10,11 +10,11 @@ print(fruits)
 fruits.append(input('Give me another fruit > '))
 print(fruits)
 
-# number = int(input('Give me a number > '))
-# print(number, fruits[number-1])
+number = int(input('Give me a number > '))
+print(number, fruits[number-1])
 
-# fruits = list('Mangoes') + fruits
-# print(fruits)
+fruits = list('Mangoes') + fruits
+print(fruits)
 
 fruits.insert(0, 'Cherries')
 print(fruits)
@@ -25,50 +25,3 @@ for fruit in fruits:
 
 
 # Series 2
-
-# print(fruits)
-#
-# fruits.pop()
-# print(fruits)
-#
-# fruit_delete = input('Give me a fruit to delete > ')
-# fruits.remove(fruit_delete)
-# print(fruits)
-#
-# fruits = fruits * 2
-# print(fruits)
-#
-# while fruits:
-#     fruits.remove(input('Give me a fruit to delete >'))
-#     print(fruits)
-
-
-# Series 3
-
-# print(fruits)
-#
-# fruits_to_remove = []
-# for fruit in fruits:
-#     response = input('Do you like ' + fruit.lower() + '? (yes/no)')
-#     if response == 'no'.lower():
-#         fruits_to_remove.append(fruit)
-#         print(fruit)
-#     elif response == 'yes'.lower():
-#         print('Me too!')
-# print(fruits_to_remove)
-# for fruit in fruits_to_remove:
-#     fruits.remove(fruit)
-# print(fruits)
-
-
-# Series 4
-
-new_fruits = fruits[:]
-for fruit in new_fruits:
-    fruit = fruit[::-1] # doesn't work
-print('New fruits reversed:')
-print(new_fruits) # incorrect: not reversed
-
-fruits.pop()
-print(fruits)
-print(new_fruits)
