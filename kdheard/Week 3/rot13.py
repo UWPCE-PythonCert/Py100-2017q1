d7467af38819da65b5438f82bbba0f6e4354c4a7
Kayla Heard <kdheard@uw.edu>
2017-02-01 06:36:17 +0000

    Finished ROT13

--- kdheard/Week 3/rot13.py
+++ kdheard/Week 3/rot13.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
+plaintext = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVQXYZ"
+ciphertext = "nopqrstuvwxyzabcdefghijlkmNOPQRSTUVWXYZABCDEFGHIJKLM"
+
+def main():
+
+    menu = input("Enter 1 to encrypt, 2 to decrypt, 3 to exit > ")
+    if menu == "1":
+        rot13()
+    elif menu == "2":
+        decrypt()
+    elif menu == "3":
+        quit()
+
 
 def rot13():
-    plaintext = raw_input("Enter the text to be encrypted: ")
-    str(plaintext)
-    print(list(plaintext))
+    cipher = str.maketrans(plaintext,ciphertext)
+
+    prompt = input("Enter the text to be encrypted: ")
+    print(prompt.translate(cipher))
+    main()
+
+def decrypt():
+
+    anti_cipher = str.maketrans(ciphertext,plaintext)
+
+    prompt = input("Enter the text to be decrypted: ")
+    print(prompt.translate(anti_cipher))
+    main()
+
 
 if __name__ == "__main__":
-    rot13()
+    main()
