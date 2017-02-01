0dc7540b239960bc301e4fbf50b2c00df2896027
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-01 03:33:12 -0800

    IN PROGRESS!!!

--- vzzjav/Session_03/Exercises/mailroom.py
+++ vzzjav/Session_03/Exercises/mailroom.py
@@ -5,6 +5,14 @@
 
 import os
 
+def thanks(donors):
+    print("Type [list] to display donors list.")
+    print("Type a name to look in donors list or add a new one.")
+    name = input("Input: ")
+    if name == "list":
+        for donor in donors:
+            print(donor[0])
+
 def report(donors):
     donors.sort(key = lambda x: x[1], reverse=True)
     print("{0:<20}{1:>13}{2:^20}{3:>25}".format("Name", "Total Donated", "No. of Donations", "Avg. of Donations"))
@@ -16,6 +24,7 @@ def main():
     donors = [["Maximus Decimus", 1000, 1, 1000/1], ["William Wallace", 2000, 2, 2000/2],
               ["Joan of Arc", 3500, 3, 3500/3], ["Bruce Wayne", 1000000, 1, 1000000/1]]
     report(donors)
+    thanks(donors)
 
 
 if __name__ == '__main__':
