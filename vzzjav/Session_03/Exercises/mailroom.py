28b704c234a195c32b18355b2bc2787f55de0bcf
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-01 18:47:42 -0800

    IN PROGRESS!!!

--- vzzjav/Session_03/Exercises/mailroom.py
+++ vzzjav/Session_03/Exercises/mailroom.py
@@ -5,7 +5,7 @@
 
 import os
 
-def thanks(donors):
+def donation(donors):
     donation = 0
     print("Type [list] to display donors list.")
     print("Type a name to look in donors list or add a new one.")
@@ -13,6 +13,7 @@ def thanks(donors):
     if name == "list":
         for donor in donors:
             print(donor[0])
+        return donors
     elif name not in [donor[0] for donor in donors if name in donor]:
         print("New donor: ", name)
         while True:
@@ -22,19 +23,40 @@ def thanks(donors):
             except ValueError:
                 print("Please provide a numeric value!")
         donors.append([name, donation, 1, donation/1])
-        print(donors)
+        thanks(len(donors) - 1, donation, donors)
+        return donors
     elif name in [donor[0] for donor in donors if name in donor]:
         print("Donor: ", name)
-        [print(donor[0]) for donor in donors if name in donor]
         while True:
             try:
                 donation = int(input("Donation: "))
                 break
             except ValueError:
                 print("Please provide a numeric value!")
-        index = [print(i) for i in range(len(donors)) if name in donors[i][0]]
-        # donors.append([name, donation, 1, donation / 1])
-        print(len(index))
+        index = [i for i in range(len(donors)) if name in donors[i][0]]
+        index = index[0]
+        donors[index][1] += donation
+        donors[index][2] += 1
+        donors[index][3] = donors[index][1]/donors[index][2]
+        thanks(index, donation, donors)
+        return donors
+
+def thanks(index, donation, donors):
+    print('''{0},
+
+    On behalf of XYZ Organization, I would like to thank you
+    for your contribution of ${1}. Your financial support helps
+    us continue in our mission and the assist those in our community.
+
+    The generous support of individuals like you makes it possible
+    for our organization to exist and to make the community a great
+    place to live.
+
+    Thank you again for your support!
+
+    Sincerely,
+    XYZ Organization\n'''.format(donors[index][0], donation))
+
 
 def report(donors):
     donors.sort(key = lambda x: x[1], reverse=True)
@@ -47,7 +69,11 @@ def main():
     donors = [["Maximus Decimus", 1000, 1, 1000/1], ["William Wallace", 2000, 2, 2000/2],
               ["Joan of Arc", 3500, 3, 3500/3], ["Bruce Wayne", 1000000, 1, 1000000/1]]
     report(donors)
-    thanks(donors)
+    while True:
+        donors = donation(donors)
+        if input("Type [quit] to quit.") == "quit":
+            break
+        report(donors)
 
 
 if __name__ == '__main__':
