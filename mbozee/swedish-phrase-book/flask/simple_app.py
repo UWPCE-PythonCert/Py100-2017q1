591d876ab4a3e90cb233c75726e018e165b0b0bb
Mike Bozee <msbozee@gmail.com>
2017-02-04 22:29:54 +0000

    swedish phrase book, flask

--- mbozee/swedish-phrase-book/flask/simple_app.py
+++ mbozee/swedish-phrase-book/flask/simple_app.py
@@ -13,6 +13,6 @@ def index(name="Treehouse"):
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
-    return "{} + {} = {}".format(num1, num2, num1+num2)
+    return num1 + num2
 
 app.run(debug=True, port=8000, host='0.0.0.0')
