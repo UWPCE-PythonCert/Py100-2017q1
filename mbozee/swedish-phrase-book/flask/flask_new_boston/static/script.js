416627558d0f0698afd2cf5a0309c823c90ab23e
Mike Bozee <msbozee@gmail.com>
2017-02-05 14:39:28 +0000

    swedish: correct arrow color

--- mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
+++ mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
@@ -2,12 +2,22 @@
  * Created by vagrant on 2/5/17.
  */
 
-// Input turns aqua if answer is correct
+// Input turns green if answer is correct
 var score = 0;
 $('.vocab-list input').keyup(function () {
     if($(this).val()==$(this).attr('placeholder')) {
-        $(this).css({'background-color': 'aqua', 'color': '#fff'});
+        $(this).css({'background-color': 'limegreen', 'color': '#fff'});
+        $(this).parent('li').addClass('correct');
         score++;
         $('#score span').html(score);
     }
 });
+
+
+
+// Enable enter key for input transversing
+// $('.vocab-list input').on('keydown', 'input', function (event) {
+//     if (event.which == 13) {
+//         event.preventDefault();
+//     }
+// });
