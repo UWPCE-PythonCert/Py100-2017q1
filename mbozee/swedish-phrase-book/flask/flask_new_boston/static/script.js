6009a87d8e5635bf63b6f1ac2896ad267e353c5b
Mike Bozee <msbozee@gmail.com>
2017-02-05 06:14:12 +0000

    correct answer font color white

--- mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
+++ mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
@@ -5,6 +5,6 @@
 // Input turns aqua if answer is correct
 $('.vocab-list input').keyup(function () {
     if($(this).val()==$(this).attr('placeholder')) {
-        $(this).css('background-color', 'aqua');
+        $(this).css({'background-color': 'aqua', 'color': '#fff'});
     }
 });
