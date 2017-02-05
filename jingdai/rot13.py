ff77373174abb74f5a52a2d359809bdde05ba929
ggplot123 <daisally@hotmail.com>
2017-02-05 23:35:33 +0000

    week four homework

--- jingdai/rot13.py
+++ jingdai/rot13.py
@@ -1,13 +1,15 @@
 #rot13
-
 import string
-def rot13(string):
-    encoding = string.maketrans(
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+if __name__ == '__main__':
 
 
-    return print(string.translate(encoding))
+    def rot13(string):
+        encoding = string.maketrans(
+        "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+
+
+        return print(string.translate(encoding))
 
 
 rot13('Zntargvp sebz bhgfvqr arne pbeare')
