a28d1416a5790da67509339235ec26ec247c9f83
Mike Bozee <msbozee@gmail.com>
2017-02-05 06:03:26 +0000

    input validation if correct answer

---
+++ mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vagrant on 2/5/17.
+ */
+
+// Input turns aqua if answer is correct
+$('.vocab-list input').keyup(function () {
+    if($(this).val()==$(this).attr('placeholder')) {
+        $(this).css('background-color', 'aqua');
+    }
+});
