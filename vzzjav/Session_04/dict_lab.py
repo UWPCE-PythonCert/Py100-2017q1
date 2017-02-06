b9ae33e1000ff31a05b2c12b69273892641baa74
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-05 22:16:03 -0800

    IN PROGRESS!!!

---
+++ vzzjav/Session_04/dict_lab.py
@@ -0,0 +1,14 @@
+# Javier Vazquez
+# Grid Printer
+# Jan 29, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/dict_lab.html
+
+import os
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__))
+    print(main.__doc__)
+    main()
