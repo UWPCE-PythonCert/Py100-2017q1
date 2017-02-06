6ab7285cb89fd4436556584145e54ba3c477308a
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-06 04:32:14 -0800

    IN PROGRESS!!!

--- vzzjav/Session_04/dict_lab.py
+++ vzzjav/Session_04/dict_lab.py
@@ -7,6 +7,16 @@ import os
 
 def main():
     cakeDict = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+    print(cakeDict)
+    del cakeDict["cake"]
+    print(cakeDict)
+    cakeDict["fruit"] = "Mango"
+    print(cakeDict)
+    print(cakeDict.keys())
+    print(cakeDict.values())
+    print("cake" in cakeDict.keys())
+    print("Mango" in cakeDict.values())
+
 
 if __name__ == '__main__':
     print(os.path.basename(__file__))
