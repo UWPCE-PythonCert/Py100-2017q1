3531759a1153f1e49d295ffb87013f549eea3884
Jesse Follet <jesse.follet@yahoo.com>
2017-02-06 19:32:06 +0000

    finished add entry

--- follej/session04/dict_lab.py
+++ follej/session04/dict_lab.py
@@ -6,7 +6,26 @@ d = dict(name='Chris', city='Seattle', cake='Chocolate')
 def display_dictionary():
     for k, v in d.items():
         print("%s: %s" % (k, v))
+    print("")
+
+
+def delete_item(param):
+    d.pop(param)
+
+
+def add_a_dict_entry(param):
+    d.update(param)
+
+
+def is_in_dictionary(param):
+    print("%s is %sin dictionary" % (param, ("" if param in d else "not ")))
 
 
 if __name__ == '__main__':
     display_dictionary()
+    delete_item('cake')
+    display_dictionary()
+    add_a_dict_entry({'fruit': 'Mango'})
+    display_dictionary()
+    is_in_dictionary('cake')
+    is_in_dictionary('fruit')
