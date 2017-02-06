10df4ac0e306635d144ff92915d32dc7969aca76
Mike Bozee <msbozee@gmail.com>
2017-02-06 03:26:50 +0000

    swd: incorrect input validation, timer

--- mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
+++ mbozee/swedish-phrase-book/flask/flask_new_boston/static/script.js
@@ -4,14 +4,35 @@
 
 // Input turns green if answer is correct
 var score = 0;
-$('.vocab-list input').keyup(function () {
+$('.vocab-list input').change(function () {
     if($(this).val()==$(this).attr('placeholder')) {
         $(this).css({'background-color': '#00d187', 'color': '#fff'});
+        $(this).prop('disabled', true);
         $(this).parent('li').addClass('correct');
         score++;
         $('#score span').html(score);
     }
+    else {
+        $(this).css({'background-color': '#FF5300', 'color': '#fff'});
+        $(this).prop('disabled', true);
+        $(this).parent('li').addClass('incorrect');
+    }
 });
+
+
+
+// Countdown timer
+function timer() {
+    var time = 5;
+    $('#timer').html(time);
+    time--;
+    if (time < 0) {
+        alert('You lose!');
+    }
+    else {
+        setTimeout(timer, 1000);
+    }
+}
 
 
 
