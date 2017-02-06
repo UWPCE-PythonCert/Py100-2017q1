16c700a650d11799d4f05612ca1147b799c13d17
Jesse Follet <jesse.follet@yahoo.com>
2017-02-06 21:49:24 +0000

    Finished dict_lab.py

--- follej/session04/dict_lab.py
+++ follej/session04/dict_lab.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+d = {}
 
-d = dict(name='Chris', city='Seattle', cake='Chocolate')
+
+def create_initial_dictionary():
+    global d
+    d = dict(name='Chris', city='Seattle', cake='Chocolate')
 
 
-def display_dictionary():
-    for k, v in d.items():
+def display_dictionary(param):
+    for k, v in param.items():
         print("%s: %s" % (k, v))
     print("")
 
@@ -19,13 +23,26 @@ def add_a_dict_entry(param):
 
 def is_in_dictionary(param):
     print("%s is %sin dictionary" % (param, ("" if param in d else "not ")))
+    print("")
+
+
+def make_letter_count_dictionary(param):
+    d2 = {}.fromkeys(d)
+    for key in d2:
+        d2[key] = d[key].count(param)
+    return d2
 
 
 if __name__ == '__main__':
-    display_dictionary()
+    create_initial_dictionary()
+    display_dictionary(d)
     delete_item('cake')
-    display_dictionary()
+    display_dictionary(d)
     add_a_dict_entry({'fruit': 'Mango'})
-    display_dictionary()
+    display_dictionary(d)
     is_in_dictionary('cake')
     is_in_dictionary('fruit')
+    create_initial_dictionary()
+    display_dictionary(d)
+    d2 = make_letter_count_dictionary('t')
+    display_dictionary(d2)
