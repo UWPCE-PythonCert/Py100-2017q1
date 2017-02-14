85233b65e5df7e971fc2011c12ddbdd22fa905e8
Mike Bozee <msbozee@gmail.com>
2017-02-14 04:08:00 +0000

    mailroom flask heroku deploy

--- mbozee/mailroom_ui/app.py
+++ mbozee/mailroom_ui/app.py
@@ -19,7 +19,7 @@ def index():
     for donor in donors_sorted:
         donor_name = donor[0]
         total_given = sum(donor[1])
-        num_gifts= len(donor[1])
+        num_gifts = len(donor[1])
         avg_gift = round(sum(donor[1])/len(donor[1]))
 
     return render_template("index.html", donors=donors, donors_sorted=donors_sorted, donor_name=donor_name, total_given=total_given, num_gifts=num_gifts, avg_gift=avg_gift)
