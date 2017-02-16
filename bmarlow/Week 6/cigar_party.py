ee8cdc35cd0f4f1b159b890b9e1abc12f0b4967b
buddinski <brandon.n.marlow@gmail.com>
2017-02-15 20:17:54 -0800

    Oooh, I can do this smarter
    fewer lines

--- bmarlow/Week 6/cigar_party.py
+++ bmarlow/Week 6/cigar_party.py
@@ -15,10 +15,7 @@ def cigar_party(cigars, is_weekend):
     if is_weekend == True:
         if cigars >= 40:
             return True
-        else:
-            return False
-    if is_weekend == False:
+    elif is_weekend == False:
         if cigars >= 40 and cigars <= 60:
             return True
-        else:
-            return False
+    return False
