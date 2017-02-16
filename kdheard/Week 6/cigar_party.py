ee0861f22cc5ae23d486c4ec83eacbaf866f17f1
Kayla Heard <kdheard@uw.edu>
2017-02-16 16:14:43 +0000

    cigar party

---
+++ kdheard/Week 6/cigar_party.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between
+40 and 60, inclusive. Unless it is the weekend, in which case there
+is no upper bound on the number of cigars.
+
+Return True if the party with the given values is successful,
+or False otherwise.
+"""
+
+
+def cigar_party(cigars, is_weekend):
+    while True:
+        try:
+            if cigars <= 39:
+                return False
+            if is_weekend == True:
+                return True
+            if is_weekend ==False:
+                if cigars <= 60:
+                    return True
+                if cigars >= 60:
+                    return False
+        except TypeError as error:
+            return TypeError
