808c8b166d8ee1469aa0d7089b038c0640d47b7b
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-15 16:08:39 -0800

    IN PROGRESS!!!

---
+++ vzzjav/Session_05/dict_set_com.py
@@ -0,0 +1,14 @@
+# Javier Vazquez
+# Grid Printer
+# Feb 15, 2017
+# Description: http://uwpce-pythoncert.github.io/IntroPython2016a/exercises/comprehensions_lab.html
+import os
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__))
+    print(main.__doc__)
+    main()
