a4eab26486bc290895e0cdebeebff0ac39a6b508
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-17 12:48:39 -0800

    WORKS OK!!!

--- vzzjav/Session_05/dict_set_com.py
+++ vzzjav/Session_05/dict_set_com.py
@@ -9,8 +9,10 @@ def main():
     food_prefs = {"name":"Chris", "city":"Seattle", "cake":"chocolate", "fruit":"mango",
                "salad":"greek", "pasta":"lasagna"}
 
-    print("{0} is from {1}, and he likes{2}{3}"
-          .format(food_prefs["name"], food_prefs["city"], food_prefs["cake"], food_prefs["cake"].keys()))
+    print("\033[1m {0} is from {1}, and he likes {2} {3}, {4} {5}, \n \033[0m {6} {7}, and {8} {9}"
+          .format(food_prefs["name"], food_prefs["city"], food_prefs["cake"], "cake",
+                  food_prefs["fruit"], "fruit", food_prefs["salad"], "salad", food_prefs["pasta"],
+                  "pasta"))
 
 
 if __name__ == '__main__':
