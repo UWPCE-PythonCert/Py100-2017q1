a6a9a901c79044e91e66e0f9c1fc48ab67d7a1be
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-21 15:49:02 -0800

    WORKS OK!!!

--- vzzjav/Session_06/test_trapz.py
+++ vzzjav/Session_06/test_trapz.py
@@ -9,7 +9,7 @@ def line(x):
     '''a very simple straight horizontal line at y = 5'''
     return 5
 
-def trapz(fun, a, b):
+def trapz(fun, a, b, n=100):
     """
     Compute the area under the curve defined by
     y = fun(x), for x between a and b
@@ -23,13 +23,13 @@ def trapz(fun, a, b):
     :param b: the end point for the integration
     :type b: a numeric value
     """
-    increase = (b-a)/100
+    increase = (b-a)/n
     print(increase)
     print("hola")
     increment = a
     print(increment)
     while increment < b:
-        increment += .1
+        increment += increase
         print(increment)
     return (b-a)*fun
 
