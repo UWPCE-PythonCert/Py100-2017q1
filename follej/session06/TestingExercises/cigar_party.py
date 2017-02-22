dd512c45b071bd164ec24f218f4ad99523388639
Jesse Follet <jesse.follet@yahoo.com>
2017-02-22 16:56:13 +0000

    Added session6 testing assignments

---
+++ follej/session06/TestingExercises/cigar_party.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between
+40 and 60, inclusive. Unless it is the weekend, in which case there
+is no upper bound on the number of cigars.
+
+Return True if the party with the given values is successful,
+or False otherwise.
+"""
+
+
+def cigar_party(cigars, is_weekend):
+    # return (False, True)[(is_weekend and cigars>=40) or (cigars >=40 and cigars <=60)]
+
+    if (is_weekend and cigars>=40) or (cigars >=40 and cigars <=60):
+    	return True
+    else:
+    	return False
+
+ #    if (is_weekend and cigars>=40) 
+ #    	return True
+	# elif (cigars >=40 and cigars <=60):
+ #    	return True
+ #    else:
+ #    	return False
