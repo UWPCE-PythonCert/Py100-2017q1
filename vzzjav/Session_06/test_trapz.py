8f867335332761957cfdb4f0d993f61f59b1c823
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-22 04:45:26 -0800

    IN PROGRESS!!!

--- vzzjav/Session_06/test_trapz.py
+++ vzzjav/Session_06/test_trapz.py
@@ -10,20 +10,13 @@ def line(x):
     return 5
 
 def area(x):
-    n = 100
-    interval = x/n
-    aux = []
-    for i in range(n):
-        if i == 0:
-            aux.append(interval)
-        elif i == 99:
-            aux.append(interval)
-        else:
-            aux.append(interval)
-    return sum(aux)
+    return ((x[len(x)-1]-x[0])/len(x))*((x[0]+x[len(x)-1])/2+sum(x[1:99]))
+
+def quadratic(x, A=0, B=0, C=0):
+    return A * x**2 + B * x + C
 
 
-def trapz(fun, a, b):
+def trapz(fun, a, b, A=0, B=0, C=0):
     """
     Compute the area under the curve defined by
     y = fun(x), for x between a and b
@@ -37,14 +30,17 @@ def trapz(fun, a, b):
     :param b: the end point for the integration
     :type b: a numeric value
     """
-
-    return fun(b-a)
+    n = 100
+    interval = (b-a)/n
+    aux = [a if i == 0 else b if i == 99 else interval*i for i in range(n)]
+    return fun(aux)
+    # return fun(b-a, A, B, C)
 
 def main():
     result = trapz(area, 0, 10)
     print(result)
-
-
+    # result = trapz(quadratic, 2, 20, A=1, B=3, C=2)
+    # print(result)
 
 if __name__ == '__main__':
     print(os.path.basename(__file__))
