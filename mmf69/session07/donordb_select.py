b8534b1061cb7d16ed59c50ee41ce52ccf0ec1f8
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-02-26 19:05:49 -0800

    Use prettytable to create Donor Report. Need to figure out how to add a title row without having to use a print()

--- mmf69/session07/donordb_select.py
+++ mmf69/session07/donordb_select.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
+from prettytable import PrettyTable
 import sqlite3
+
 
 conn = sqlite3.connect('donor.db')
 print("Opened database successfully")
@@ -19,6 +21,28 @@ for row in cursor:
     print("TOTAL_DONATIONS = ", row[3])
     print("DONATION_AMOUNT = ", row[2])
     print("AVERAGE_DONATION = ", row[4], "\n")
+
+
+print("**********************DONOR REPORT*******************")
+
+cursor = conn.execute("SELECT DONOR_ID as 'Donor ID',DONOR_NAME as 'Donor Name',"
+                      "total(DONATION_AMOUNT) AS 'Total Donations',"
+                      "count(distinct DONATION_AMOUNT) AS 'Number of Donations',"
+                      "total(DONATION_AMOUNT)/count(distinct DONATION_AMOUNT) AS AVERAGE_DONATION"
+                      " from donors group by DONOR_ID,DONOR_NAME")
+
+col_names = [cn[0] for cn in cursor.description]
+rows = cursor.fetchall()
+
+x = PrettyTable(col_names)
+x.align[col_names[1]] = "l"
+x.align[col_names[2]] = "r"
+x.padding_width = 1
+for row in rows:
+    x.add_row(row)
+
+print(x)
+
 
 print("Operation done successfully")
 
