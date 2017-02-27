76c847c729113774fea47e65d7416a3fd6e77d22
Jesse Follet <jesse.follet@yahoo.com>
2017-02-27 00:29:30 +0000

    Finished ConvertRomanArabic.  Passes all tests.

--- follej/session07/ConvertRomanArabic.py
+++ follej/session07/ConvertRomanArabic.py
@@ -1,19 +1,32 @@
+import collections
+
+
 class ConvertRomanArabic:
-    roman_numbers = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-    roman_ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    roman_dict = collections.OrderedDict(
+        zip(['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'],
+            [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]))
 
     # def __init__(self):
 
     def arabic_to_roman(self, arabic_number):
         roman_number = []
-        for i in range(len(self.roman_ints)):
-            total = int(arabic_number / self.roman_ints[i])
-            roman_number.append(self.roman_numbers[i] * total)
-            arabic_number -= self.roman_ints[i] * total
+        for roman_numeral, roman_integer in self.roman_dict.items():
+            total = int(arabic_number / roman_integer)
+            roman_number.append(roman_numeral * total)
+            arabic_number -= roman_integer * total
         return ''.join(roman_number)
 
-    def roman_to_arabic(self, roman_num):
-        return None
+    def roman_to_arabic(self, roman_numeral):
+        roman_str = str(roman_numeral)
+        total = 0
+        for i, roman_number in enumerate(roman_str):
+            value = self.roman_dict[roman_number]
+            if i + 1 < len(roman_str) and self.roman_dict[roman_str[i + 1]] > value:
+                total -= value
+            else:
+                total += value
+        return total
 
 
-print(ConvertRomanArabic().arabic_to_roman(999))
+print(ConvertRomanArabic().arabic_to_roman(1999))
+print(ConvertRomanArabic().roman_to_arabic('III'))
--- follej/session07/test_ConvertRomanArabic.py
+++ follej/session07/test_ConvertRomanArabic.py
@@ -4,7 +4,15 @@ from ConvertRomanArabic import ConvertRomanArabic
 
 def test_arabic_to_roman():
     assert ConvertRomanArabic().arabic_to_roman(999) == 'CMXCIX'
+    assert ConvertRomanArabic().arabic_to_roman(1999) == 'MCMXCIX'
+    assert ConvertRomanArabic().arabic_to_roman(13) == 'XIII'
 
 
 def test_roman_to_arabic():
-    assert ConvertRomanArabic.roman_to_arabic('IV') == 4
+    assert ConvertRomanArabic().roman_to_arabic('IV') == 4
+    assert ConvertRomanArabic().roman_to_arabic('MCMXCIX') == 1999
+
+
+def test_both_conversions():
+    assert ConvertRomanArabic().arabic_to_roman(ConvertRomanArabic().roman_to_arabic('MCMXCIX')) == 'MCMXCIX'
+    assert ConvertRomanArabic().roman_to_arabic(ConvertRomanArabic().arabic_to_roman(1999)) == 1999
