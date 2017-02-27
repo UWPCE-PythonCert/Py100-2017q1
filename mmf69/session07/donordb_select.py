b94d54abdd3bf481218b90b5eaae37198a807e41
Feliciano, Matthew <Matthew.Feliciano@cdk.com>
2017-02-27 12:51:16 -0800

    Revert donordb_select.py

--- mmf69/session07/donordb_select.py
+++ mmf69/session07/donordb_select.py
@@ -3,8 +3,25 @@
 from prettytable import PrettyTable
 import sqlite3
 
+
 conn = sqlite3.connect('donor.db')
 print("Opened database successfully")
+
+cursor = conn.execute("SELECT DONOR_ID,DONOR_NAME,DONATION_AMOUNT from DONORS")
+for row in cursor:
+    print(row)
+
+
+cursor = conn.execute("SELECT DONOR_ID,DONOR_NAME,total(DONATION_AMOUNT),"
+                      "count(distinct DONATION_AMOUNT),total(DONATION_AMOUNT)/count(distinct DONATION_AMOUNT)"
+                      " from donors group by DONOR_ID,DONOR_NAME")
+for row in cursor:
+    print("DONOR_ID = ", row[0])
+    print("DONOR_NAME = ", row[1])
+    print("TOTAL_DONATIONS = ", row[3])
+    print("DONATION_AMOUNT = ", row[2])
+    print("AVERAGE_DONATION = ", row[4], "\n")
+
 
 print("*********************************DONOR REPORT***************************************")
 
@@ -25,6 +42,7 @@ for row in rows:
     x.add_row(row)
 
 print(x)
+
 
 print("Operation done successfully")
 
